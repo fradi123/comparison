b476179efe7b77a2dfa11e5307b519b0f6bcb6c7
fradi123 <158277503+fradi123@users.noreply.github.com>
2024-05-08 12:27:55 +0100

    W6 adding react props

--- react-App/src/App.jsx
+++ react-App/src/App.jsx
@@ -224,195 +224,88 @@
   
 
 
-// const App = () => 
+ const App = () => 
 
-//   (
+   (
  
-//       <div>
-//         <h1> My Hacker-stories </h1>
-//         <Search/>
-//         <hr/>
-//         <List/>
-//       </div>
-//   )
+       <div>
+         <h1> My Hacker-stories </h1>
+         <Search/>
+         <hr/>
+         <List/>
+       </div>
+   )
 
 
-// export default App
+ export default App
 
-// import * as React from "react";
-
-
-// const List = (props) => 
-//   (
-//     <ul>
-//           {props.list.map((item) => (
-//           <Item key={item.objectID} item={item} />
-//           ))}
-//         </ul>
-//   );
+ import * as React from "react";
 
 
-// const Item = (props) => (
-//   <li>
-//     <span>
-//         <a href={props.item.url}> {props.item.title} </a>
-//     </span>
-//     <span> {props.item.author} </span>
-//     <span> {props.item.num_comments} </span>
-//     <span> {props.item.points} </span>
-//   </li>
-// );
+ const List = (props) => 
+   (
+     <ul>
+           {props.list.map((item) => (
+           <Item key={item.objectID} item={item} />
+           ))}
+         </ul>
+   );
 
 
-// const Search = () => {
+ const Item = (props) => (
+   <li>
+     <span>
+         <a href={props.item.url}> {props.item.title} </a>
+    </span>
+   <span> {props.item.author} </span>
+   <span> {props.item.num_comments} </span>
+    <span> {props.item.points} </span>
+   </li>
+ );
+ const App = () => 
 
-//   const handleChange = (event) => {
-    
-//     console.log(event);
-    
-//     console.log(event.target.value);
-//     };
-    
+  (
 
-//   return (
-//     <div>
-//     <label htmlFor="search">Search: </label>
-//     <input id="search" type="text" onChange={handleChange} />
-//   </div>
-//   );
-
-// }
-  
-  
+      <div>
+        <h1> My Hacker-stories </h1>
+        <Search/>
+        <hr/>
+        <List/>
+      </div>
+  )
 
 
-// const App = () => 
-// {
-//   const stories = [
-//     {
-//       title: "React",
-//       url: "https://reactjs.org",
-//       author: "Jordan Walke",
-//       num_comments: 3,
-//       points: 4,
-//       objectID: 0,
-  
-//     },
-//     {
-//       title: "Redux",
-//       url: "https://redux.js.org",
-//       author: "Dan Abramov",
-//       num_comments: 2,
-//       points: 5,
-//       objectID: 1,
-//     }
-//   ]
-//   return(
- 
-//       <div>
-//         <h1> My Hacker-stories </h1>
-//         <Search/>
-//         <hr/>
-//         <List list={stories}/>
-//       </div>
-//   );
-//   };
-
-// export default App
+export default App
 
 import * as React from "react";
 
 
-const List = (props) => {
-  console.log("LIST component RENDERED");
-  return(
+const List = (props) => 
+  (
     <ul>
           {props.list.map((item) => (
           <Item key={item.objectID} item={item} />
           ))}
         </ul>
   );
-          };
 
-const Item = (props) => {
-  console.log("ITEM component RENDERED");
-  return(
+
+const Item = (props) => (
   <li>
     <span>
         <a href={props.item.url}> {props.item.title} </a>
-    </span>
-    <span> {props.item.author} </span>
-    <span> {props.item.num_comments} </span>
-    <span> {props.item.points} </span>
+   </span>
+  <span> {props.item.author} </span>
+  <span> {props.item.num_comments} </span>
+   <span> {props.item.points} </span>
   </li>
-);};
+);
 
 
-const Search = () => {
-
-  console.log("SEARCH component RENDERED")
-  const [searchTerm, setSearchTerm] = React.useState('');
-
-  const handleChange = (event) => {
-    
-    console.log(event);
-    
-    console.log(event.target.value);
-
-    setSearchTerm(event.target.value);
-    };
-    
-
-  return (
-    <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" onChange={handleChange} />
-
-    <p>
-      Searching for <strong>{searchTerm}</strong>
-    </p>
-  </div>
-  );
-
-}
-  
-  
 
 
-const App = () => 
-{
-  console.log("APP component RENDERED");
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-  
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org",
-      author: "Dan Abramov",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    }
-  ]
-  return(
- 
-      <div>
-        <h1> My Hacker-stories </h1>
-        <Search/>
-        <hr/>
-        <List list={stories}/>
-      </div>
-  );
-  };
 
-export default App
+
 
 
 
