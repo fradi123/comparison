ab5d3a9ed9d205d4cfe17272062d37ac68a8b83a
fradi123 <158277503+fradi123@users.noreply.github.com>
2024-05-18 14:21:24 +0100

    W6 filtering the list

--- react-App/src/App.jsx
+++ react-App/src/App.jsx
@@ -224,111 +224,176 @@
   
 
 
- const App = () => 
+//  const App = () => 
 
-   (
+//    (
  
-       <div>
-         <h1> My Hacker-stories </h1>
-         <Search/>
-         <hr/>
-         <List/>
-       </div>
-   )
+//        <div>
+//          <h1> My Hacker-stories </h1>
+//          <Search/>
+//          <hr/>
+//          <List/>
+//        </div>
+//    )
 
 
- export default App
+//  export default App
 
- import * as React from "react";
-
-
- const List = (props) => 
-   (
-     <ul>
-           {props.list.map((item) => (
-           <Item key={item.objectID} item={item} />
-           ))}
-         </ul>
-   );
+//  import * as React from "react";
 
 
- const Item = (props) => (
-   <li>
-     <span>
-         <a href={props.item.url}> {props.item.title} </a>
-    </span>
-   <span> {props.item.author} </span>
-   <span> {props.item.num_comments} </span>
-    <span> {props.item.points} </span>
-   </li>
- );
- const App = () => 
-
-  (
-
-      <div>
-        <h1> My Hacker-stories </h1>
-        <Search/>
-        <hr/>
-        <List/>
-      </div>
-  )
+//  const List = (props) => 
+//    (
+//      <ul>
+//            {props.list.map((item) => (
+//            <Item key={item.objectID} item={item} />
+//            ))}
+//          </ul>
+//    );
 
 
-export default App
+//  const Item = (props) => (
+//    <li>
+//      <span>
+//          <a href={props.item.url}> {props.item.title} </a>
+//     </span>
+//    <span> {props.item.author} </span>
+//    <span> {props.item.num_comments} </span>
+//     <span> {props.item.points} </span>
+//    </li>
+//  );
+//  const App = () => 
 
+//   (
+
+//       <div>
+//         <h1> My Hacker-stories </h1>
+//         <Search/>
+//         <hr/>
+//         <List/>
+//       </div>
+//   )
+
+
+// export default App
+
+// import * as React from "react";
+
+
+// const List = (props) => 
+//   (
+//     <ul>
+//           {props.list.map((item) => (
+//           <Item key={item.objectID} item={item} />
+//           ))}
+//         </ul>
+//   );
+
+
+// const Item = (props) => (
+//   <li>
+//     <span>
+//         <a href={props.item.url}> {props.item.title} </a>
+//    </span>
+//   <span> {props.item.author} </span>
+//   <span> {props.item.num_comments} </span>
+//    <span> {props.item.points} </span>
+//   </li>
+// );
+
+// const Search = () => {
+
+//   console.log("SEARCH component RENDERED")
+//   const [searchTerm, setSearchTerm] = React.useState('');
+
+//   const handleChange = (event) => {
+    
+//     console.log(event);
+    
+//     console.log(event.target.value);
+
+//     setSearchTerm(event.target.value);
+//     };
+    
+
+//   return (
+//     <div>
+//     <label htmlFor="search">Search: </label>
+//     <input id="search" type="text" onChange={handleChange} />
+
+//     <p>
+//       Searching for <strong>{searchTerm}</strong>
+//     </p>
+//   </div>
+//   );
+
+// }
+// const [searchTerm, setSearchTerm] = React.useState("");
+//   const handleSearch = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+//   const searchedStories = stories.filter((story) =>
+//     story.title.toLowerCase().includes(searchTerm.toLowerCase()),
+//   );
+//   return (
+//     <div>
+//       <h1>My Hacker Stories</h1>
+//       <Search onSearch={handleSearch} />
+//       <hr />
+//       <List list={searchedStories} />
+//     </div>
+//   );
+
+// export default App;
 import * as React from "react";
-
-
-const List = (props) => 
-  (
-    <ul>
-          {props.list.map((item) => (
-          <Item key={item.objectID} item={item} />
-          ))}
-        </ul>
-  );
-
-
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
 const Item = (props) => (
   <li>
     <span>
-        <a href={props.item.url}> {props.item.title} </a>
-   </span>
-  <span> {props.item.author} </span>
-  <span> {props.item.num_comments} </span>
-   <span> {props.item.points} </span>
+      <a href={props.item.url}>{props.item.title} </a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
   </li>
 );
-
-const Search = () => {
-
-  console.log("SEARCH component RENDERED")
-  const [searchTerm, setSearchTerm] = React.useState('');
-
-  const handleChange = (event) => {
-    
-    console.log(event);
-    
-    console.log(event.target.value);
-
-    setSearchTerm(event.target.value);
-    };
-    
-
+const Search = (props) => {
   return (
     <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" onChange={handleChange} />
-
-    <p>
-      Searching for <strong>{searchTerm}</strong>
-    </p>
-  </div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" value={props.search} onChange={props.onSearch} />
+    </div>
   );
-
+};
+function getTitle(title) {
+  return title;
 }
-const [searchTerm, setSearchTerm] = React.useState("");
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  const [searchTerm, setSearchTerm] = React.useState('React');
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -338,14 +403,13 @@ const [searchTerm, setSearchTerm] = React.useState("");
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search onSearch={handleSearch} />
+      <Search search={searchTerm} onSearch={handleSearch} />
       <hr />
       <List list={searchedStories} />
     </div>
   );
-
+};
 export default App;
-
 
 
 
